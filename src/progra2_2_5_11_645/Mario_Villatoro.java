dc1f543b9051ed92422aab82d7f031e4749bf124
Mario Villatoro <Administrador@.(none)>
2011-10-07 08:57:46 -0600

    agregue la clase Mario Villatoro

---
+++ src/progra2_2_5_11_645/Mario_Villatoro.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progra2_2_5_11_645;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Mario_Villatoro {
+    
+    public static void main (String args[]){
+        
+        System.out.println("Mario Villatoro");
+        
+    }
+    
+}
